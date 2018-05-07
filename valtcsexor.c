69ecd656ff1366a3dc8c6e8d6dcd213d075f9067
Attila997 <n.tilla97@gmail.com>
2018-05-07 19:50:56 +0200

    valtozo csere kivonassal

---
+++ valtcsexor.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+  int a = 5, b = 10;
+
+  printf("a = %d and b = %d\n", a, b);
+
+  a = a ^ b;
+  b = a ^ b;
+  a = a ^ b;
+
+  printf("Csere utan, a = %d and b = %d\n", a, b);
+
+  return 0;
+}  
